2d0911c51aae086c5ca19dcbe99ec8dcd0538a9a
Greg Sewell <sewell.greg@gmail.com>
2022-05-18 16:02:22 +0100

    Create initial SpringBoot application

---
+++ src/test/java/com/waracle/cakemgr/CakeMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waracle.cakemgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeMgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
